b5aa4b2b898d17c36a22617c58bace1963dc3a05
reiko51 <71922155+reiko51@users.noreply.github.com>
2022-09-11 11:28:59 +0900

    organismの作成

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p></p>
+      <dl>
+        <dt>メール</dt>
+        <dd>1111@aaaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-1111-2222</dd>
+        <dt>会社名</dt>
+        <dd>ああああ会社</dd>
+        <dt>WebWeb</dt>
+        <dd>htmllllll.com</dd>
+      </dl>
+    </div>
+  );
+};
