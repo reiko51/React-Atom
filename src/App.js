490adb490cdf198dff09b578213d831a0741b965
reiko51 <71922155+reiko51@users.noreply.github.com>
2022-09-18 23:15:39 +0900

    templateの作成（修正）

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/atoms/templates/HeaderOnly";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
-import { DefaultLayout } from "./components/atoms/templates/DefaultLayout";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
 
 const user = {
   name: "じゃけえ",
